11762d3a8642735ba4efe05d3eab1c2d100ee2db
Sehmer Desktop <matheustae@hotmail.com>
2023-01-02 09:21:09 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `nied-blog`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
